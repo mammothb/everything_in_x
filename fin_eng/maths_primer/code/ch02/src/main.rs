64f1a817ac58a01ea628d55810c7ec5e3b7d3c4a
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-07 11:59:36 +0800

    add ch2 ex q3

---
+++ fin_eng/maths_primer/code/ch02/src/main.rs
@@ -0,0 +1,26 @@
+use ch02::integrate::{midpoint_rule, simpsons_rule, stopping_criterion, trapezoidal_rule};
+
+fn q3() {
+    /// Evaluates $ \sqrt{x} e^{-x} $.
+    fn integrand(x: f64) -> f64 {
+        x.sqrt() * (-x).exp()
+    }
+
+    fn partial<R>(rule: R) -> impl Fn(usize) -> f64
+    where
+        R: Fn(fn(f64) -> f64, f64, f64, usize) -> f64 + Copy,
+    {
+        move |n| rule(integrand, 1.0, 3.0, n)
+    }
+
+    println!("Midpoint");
+    stopping_criterion(1e-6, partial(midpoint_rule));
+    println!("Trapezoidal");
+    stopping_criterion(1e-6, partial(trapezoidal_rule));
+    println!("Simpson's");
+    stopping_criterion(1e-6, partial(simpsons_rule));
+}
+
+fn main() {
+    q3();
+}
