d3abe050c493699fea30a081e5e066d361700fe7
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-07 21:22:09 +0800

    add ch2 ex

--- fin_eng/maths_primer/code/ch02/src/main.rs
+++ fin_eng/maths_primer/code/ch02/src/main.rs
@@ -21,6 +21,26 @@ fn q3() {
     stopping_criterion(1e-6, partial(simpsons_rule));
 }
 
+fn q4() {
+    /// Evaluates $ \frac{x^{5/2}}{1 + x^2} $.
+    fn integrand(x: f64) -> f64 {
+        x.powf(2.5) / (1.0 + x * x)
+    }
+
+    fn partial<R>(rule: R) -> impl Fn(usize) -> f64
+    where
+        R: Fn(fn(f64) -> f64, f64, f64, usize) -> f64 + Copy,
+    {
+        move |n| rule(integrand, 0.0, 1.0, n)
+    }
+
+    println!("Midpoint");
+    stopping_criterion(1e-6, partial(midpoint_rule));
+    println!("Simpson's");
+    stopping_criterion(1e-7, partial(simpsons_rule));
+}
+
 fn main() {
-    q3();
+    // q3();
+    q4();
 }
