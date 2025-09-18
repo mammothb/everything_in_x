18b79d3dd4158b9ec1c527ac177e38eee5b5c7ba
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-18 22:41:15 +0800

    initialize dev-rs

---
+++ rust/dev-rs/src/main.rs
@@ -0,0 +1,55 @@
+use std::path::PathBuf;
+
+use clap::{Parser, Subcommand};
+
+#[derive(Debug, Parser)]
+#[command(name = "dev")]
+#[command(about = "Dev utility tools")]
+struct Cli {
+    #[command(subcommand)]
+    command: Commands,
+}
+
+#[derive(Debug, Subcommand)]
+enum Commands {
+    /// Lambda utilities
+    Lambda {
+        #[command(subcommand)]
+        command: Option<LambdaCommands>,
+    },
+    /// Run commands through a custom environment
+    Run,
+}
+
+#[derive(Debug, Subcommand)]
+enum LambdaCommands {
+    /// List the Lambda's dependencies
+    Deps,
+    /// Fetch Lambda function names
+    Fetch {
+        /// Path to infra definition file
+        #[arg(short, long)]
+        path: Option<PathBuf>,
+    },
+    /// Open CloudWatch logs
+    Log,
+}
+
+fn main() {
+    let args = Cli::parse();
+
+    match args.command {
+        Commands::Lambda { command } => {
+            if let Some(command) = command {
+                match command {
+                    LambdaCommands::Deps => {}
+                    LambdaCommands::Fetch { path } => {}
+                    LambdaCommands::Log => {}
+                }
+            } else {
+                println!("fzf");
+            }
+        }
+        Commands::Run => {}
+    }
+}
