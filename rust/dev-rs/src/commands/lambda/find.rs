9c54f7bac653cb60061a3c8434fb4c9a551773f9
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-21 21:09:22 +0800

    add fzf find

--- rust/dev-rs/src/commands/lambda/find.rs
+++ rust/dev-rs/src/commands/lambda/find.rs
@@ -1,11 +1,14 @@
-use anyhow::Result;
+use std::io::Write;
+use std::process::{Command, Stdio};
+
+use anyhow::{Context, Result};
 
 use crate::{
     cache::{Cache, CacheBucket, CacheEntry},
     config::LambdaConfig,
 };
 
-pub(crate) fn find(config: &LambdaConfig, cache: &Cache) -> Result<()> {
+pub(crate) fn find(config: &LambdaConfig, cache: &Cache) -> Result<String> {
     let LambdaConfig {
         environment,
         suffix,
@@ -15,8 +18,22 @@ pub(crate) fn find(config: &LambdaConfig, cache: &Cache) -> Result<()> {
     let file = format!("{}{}.json", environment, suffix);
     let cache_entry = cache.entry(CacheBucket::Lambda, file);
     let lambda_names = read_cache(cache_entry)?;
-    println!("{lambda_names:?}");
-    Ok(())
+
+    let mut child = Command::new("fzf")
+        .stdin(Stdio::piped())
+        .stdout(Stdio::piped())
+        .spawn()?;
+    {
+        let stdin = child.stdin.as_mut().context("failed to open stdin")?;
+        for name in &lambda_names {
+            writeln!(stdin, "{name}")?;
+        }
+    }
+
+    let output = child.wait_with_output()?;
+    let selected = String::from_utf8(output.stdout)?.trim().to_string();
+
+    Ok(selected)
 }
 
 fn read_cache(cache_entry: CacheEntry) -> Result<Vec<String>> {
