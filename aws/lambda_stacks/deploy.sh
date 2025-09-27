c4e64c3016fbb44cc534eeedf9d55ba4e0aabb19
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-27 22:39:17 +0800

    add stack suffix to deployment

--- aws/lambda_stacks/deploy.sh
+++ aws/lambda_stacks/deploy.sh
@@ -4,13 +4,14 @@ set -euo pipefail
 
 . .env
 
-suffix="${1:+-${1}}"
+STACK_SUFFIX="${STACK_SUFFIX:-}"
 
 yq -r '.stacks[] | [.name, .template_path] | @tsv' ./definition.yml \
     | while IFS=$'\t' read name template_path; do
-        stack_name="${name}${suffix}"
+        stack_name="${name}${STACK_SUFFIX}"
         echo "Deploying '${stack_name}' using '${template_path}'"
         aws cloudformation deploy \
             --stack-name "${stack_name}" \
-            --template-file "${template_path}"
+            --template-file "${template_path}" \
+            --parameter-overrides StackSuffix="${STACK_SUFFIX}"
     done
