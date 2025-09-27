cf62b1f924579a82514f4b07992be3d3ac3780cc
mmb L <7960706+mammothb@users.noreply.github.com>
2025-09-27 22:39:31 +0800

    change config and cache dir name

--- rust/ctl-rs/crates/ctl-dirs/src/lib.rs
+++ rust/ctl-rs/crates/ctl-dirs/src/lib.rs
@@ -6,12 +6,12 @@ use etcetera::{BaseStrategy, choose_base_strategy};
 pub fn user_cache_dir() -> Option<PathBuf> {
     choose_base_strategy()
         .ok()
-        .map(|dirs| dirs.cache_dir().join("dev_cli"))
+        .map(|dirs| dirs.cache_dir().join("ctl"))
 }
 
 #[must_use]
 pub fn user_config_dir() -> Option<PathBuf> {
     choose_base_strategy()
         .ok()
-        .map(|dirs| dirs.config_dir().join("dev_cli"))
+        .map(|dirs| dirs.config_dir().join("ctl"))
 }
